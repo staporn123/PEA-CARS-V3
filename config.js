a8dc36d084c2153fe6948d5969475f2b911a9b46
staporn123 <Sataporn282537@gmail.com>
2026-06-30 00:11:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   // =========================================
   // Google Apps Script API URL
   // =========================================
-  API_URL: "https://script.google.com/macros/s/AKfycbxKiIDDruUVjVORiwOBheXqFDgavg-JkPQl-qy6uovUR8-hzK5uH-torNvY-oibL7u5/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzeAhZ3kbSnAD0Rc2zxfJL7Q4-fdBb6ju45Zu9apLK7PBUnWEEUyiiSMTcWfSHYNGUJ/exec",
 
   API_TIMEOUT: 60000,
 
